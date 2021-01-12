c27da882a38d9a2be3c3f27d91becda366f97312
Tosikat <lijun5402@163.com>
2021-01-12 15:36:06 +0800

    tottoko first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,9 @@ import styles from './index.css';
 export default memo(() => {
   return (
     <div>
-      <h1 className={styles.normal}>您好</h1>
+      <h1 className={styles.normal}>不太好，tottoko上的分支</h1>
       <h2 className={styles.lijun}>Tosikat_branch</h2>
+      <p>给你个p标签</p>
     </div>
   );
 })
